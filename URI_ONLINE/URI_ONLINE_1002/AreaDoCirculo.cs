ae381111eded3d7a5b86c9c227854e4976d74dc9
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-12 17:56:41 -0300

    O exercício 1002 foi adicionado.

---
+++ URI_ONLINE/URI_ONLINE_1002/AreaDoCirculo.cs
@@ -0,0 +1,33 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1002
+ *      OBS: para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1002
+{
+    class AreaDoCirculo
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            n = 3.14159;
+
+            double raio;
+            double area;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
