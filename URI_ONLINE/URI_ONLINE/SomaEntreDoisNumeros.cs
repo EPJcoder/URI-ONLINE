42dc5d17f8e2721fe7d942b0d4f1524f9bdb1268
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-12 17:32:58 -0300

    Resolução dos exercícios do site URI ONLINE JUDGE.

---
+++ URI_ONLINE/URI_ONLINE/SomaEntreDoisNumeros.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1001
+ *      OBS: para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE
+{
+    class SomaEntreDoisNumeros
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+            Console.ReadKey();
+        }
+    }
+}
