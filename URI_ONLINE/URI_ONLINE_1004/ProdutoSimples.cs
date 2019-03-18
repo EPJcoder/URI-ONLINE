d9c11aa138e5bde3d34de26415be4e821c53d552
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-18 14:10:11 -0300

    Exercício 1004(ProdutoSimples) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1004/ProdutoSimples.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1004
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1004
+{
+    class ProdutoSimples
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int PROD;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+            Console.ReadKey();
+        }
+    }
+}
