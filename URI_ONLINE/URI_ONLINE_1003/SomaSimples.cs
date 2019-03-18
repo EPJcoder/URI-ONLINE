71e39d2b33e7e781997b36d2f6dbc81f0bd58639
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-18 13:01:16 -0300

    Exercício 1003(SomaSimples) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1003/SomaSimples.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1003
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1003
+{
+    class SomaSimples
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int SOMA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+            Console.ReadKey();
+        }
+    }
+}
