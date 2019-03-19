ae0744c3cfff58e197509123d4f20daa0fd3e10a
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 16:51:36 -0300

    Exercício 1044(NumerosMultiplos) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1044/NumerosMultiplos.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1044
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1044
+{
+    class NumerosMultiplos
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+
+            string[] vetor;
+            vetor = Console.ReadLine().Split(' ');
+            A = int.Parse(vetor[0]);
+            B = int.Parse(vetor[1]);
+
+            if ((A % B == 0) || (B % A == 0))
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
