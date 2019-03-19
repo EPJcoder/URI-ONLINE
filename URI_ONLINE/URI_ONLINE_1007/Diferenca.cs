fe5658baf5c51c26e30ac5228f0d7fe6b5a4917b
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 00:10:14 -0300

    Exercício 1007(Diferenca) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1007/Diferenca.cs
@@ -0,0 +1,35 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1007
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1007
+{
+    class Diferenca
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int C;
+            int D;
+            int DIFERENCA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            DIFERENCA = (A * B) - (C * D);
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+            Console.ReadKey();
+        }
+    }
+}
