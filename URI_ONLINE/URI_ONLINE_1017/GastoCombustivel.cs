cf1324e20ddab2ab98ee2673f5478ab1c827e646
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 13:48:39 -0300

    Exercício 1017(GastoCombustivel) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1017/GastoCombustivel.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1017
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1017
+{
+    class GastoCombustivel
+    {
+        static void Main(string[] args)
+        {
+            int tempo;
+            int velocidade;
+            double litros;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            litros = (double)tempo * velocidade / 12;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
