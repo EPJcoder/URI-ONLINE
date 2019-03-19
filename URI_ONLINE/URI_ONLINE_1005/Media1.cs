4cadc9bac1ee7781442b82672221a699ac949a33
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-18 23:46:22 -0300

    Exercício 1005(Media1) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1005/Media1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1004
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1005
+{
+    class Media1
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double MEDIA;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = (double)((3.5) * A + (7.5) * B) / 11;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
