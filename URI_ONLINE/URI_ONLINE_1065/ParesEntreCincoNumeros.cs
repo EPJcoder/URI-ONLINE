561914cff0be8aecf2f5bc8b733c9c96a7b821c0
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 17:45:07 -0300

    Exercício 1065(ParesEntreCincoNumeros) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1065/ParesEntreCincoNumeros.cs
@@ -0,0 +1,55 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1065
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1065
+{
+    class ParesEntreCincoNumeros
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int C;
+            int D;
+            int E;
+            int somaPares = 0;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            E = int.Parse(Console.ReadLine());
+
+            if (A % 2 == 0)
+            {
+                somaPares = somaPares + 1;
+            }
+            if (B % 2 == 0)
+            {
+                somaPares = somaPares + 1;
+            }
+            if (C % 2 == 0)
+            {
+                somaPares = somaPares + 1;
+            }
+            if (D % 2 == 0)
+            {
+                somaPares = somaPares + 1;
+            }
+            if (E % 2 == 0)
+            {
+                somaPares = somaPares + 1;
+            }
+            Console.WriteLine(somaPares + " valores pares");
+            Console.ReadKey();
+        }
+    }
+}
