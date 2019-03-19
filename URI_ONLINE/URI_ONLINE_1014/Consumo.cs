9bae3ffdce3f7835ec4d174396335a2a75107f59
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 13:16:53 -0300

    Exercicio 1014(Consumo) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1014/Consumo.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1014
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1014
+{
+    class Consumo
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y;
+            double consumo;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumo = X / Y;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture) + (" km/l"));
+            Console.ReadKey();
+        }
+    }
+}
