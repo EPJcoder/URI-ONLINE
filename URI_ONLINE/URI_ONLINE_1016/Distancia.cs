3eb4dddc0f2ae393131a01358fc82c25aec54dab
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 13:39:12 -0300

    Exercício 1016(Distancia) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1016/Distancia.cs
@@ -0,0 +1,29 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1016
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1016
+{
+    class Distancia
+    {
+        static void Main(string[] args)
+        {
+            int distanciaVeiculo;
+            int tempoVeiculo;
+
+            distanciaVeiculo = int.Parse(Console.ReadLine());
+
+            tempoVeiculo = distanciaVeiculo * 2;
+
+            Console.WriteLine(tempoVeiculo + " minutos");
+            Console.ReadKey();
+        }
+    }
+}
