e9f63418b12f9b56db75a84cf9451c9f6577b422
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 16:06:07 -0300

    Exercício 1037(Intervalo) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1037/Intervalo.cs
@@ -0,0 +1,46 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1037
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1037
+{
+    class Intervalo
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero > 25 && numero <= 50)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (numero >= 0 && numero <= 25)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (numero > 75 && numero <= 100)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+            else if (numero > 50 && numero <= 75)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
