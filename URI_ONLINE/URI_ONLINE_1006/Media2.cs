976aa9841f42ebdad31948fea1e78d3da0a958e8
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-18 23:58:02 -0300

    Exercício 1006(Media2) adicionado ao projeto.

--- URI_ONLINE/URI_ONLINE_1006/Media2.cs
+++ URI_ONLINE/URI_ONLINE_1006/Media2.cs
@@ -3,29 +3,31 @@
  *      Programador: Euclydes Penteado Junior
  *      Meu GitHub: EPJcoder
  *      Linguagem utilizada: C#
- *      Exercício de número: 1005
+ *      Exercício de número: 1006
  *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
  */
 
 using System;
 using System.Globalization;
 
-namespace URI_ONLINE_1005
+namespace URI_ONLINE_1006
 {
-    class Media1
+    class Media2
     {
         static void Main(string[] args)
         {
             double A;
             double B;
+            double C;
             double MEDIA;
 
             A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            MEDIA = (double)((3.5) * A + (7.5) * B) / 11;
+            MEDIA = (double)((2 * A) + (3 * B) + (5 * C)) / 10;
 
-            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", CultureInfo.InvariantCulture));
             Console.ReadKey();
         }
     }
