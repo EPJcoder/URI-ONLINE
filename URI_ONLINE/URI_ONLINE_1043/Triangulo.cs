8e65743366c1853d486521e6073ac9254e796ebe
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 16:37:22 -0300

    Exercício 1043(Triangulo) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1043/Triangulo.cs
@@ -0,0 +1,45 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1043
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1043
+{
+    class Triangulo
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double C;
+            double perimetro;
+            double area;
+
+            string[] vetor;
+            vetor = Console.ReadLine().Split(' ');
+
+            A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            if (A < B + C && A > Math.Abs(B - C))
+            {
+                perimetro = (A + B + C);
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                area = (A + B) * C / 2.0;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            Console.ReadKey();
+        }
+    }
+}
