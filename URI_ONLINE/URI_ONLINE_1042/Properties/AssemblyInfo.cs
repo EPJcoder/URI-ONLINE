4521cee6169c92f634a93565a0ab1ece1a933232
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 16:32:07 -0300

    Exercício 1042(SortSimples) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1042/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("URI_ONLINE_1042")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("URI_ONLINE_1042")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("9dc6eab1-cf9e-4b63-9f85-50f9a1fbcd00")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ URI_ONLINE/URI_ONLINE_1042/SortSimples.cs
@@ -0,0 +1,87 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1042
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1042
+{
+    class SortSimples
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+
+            string[] vetor;
+            vetor = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vetor[0]);
+            y = int.Parse(vetor[1]);
+            z = int.Parse(vetor[2]);
+
+            if (x < y && x < z && y < z)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                Console.WriteLine(z);
+            }
+            else
+            {
+                if (y < x && y < z && x < z)
+                {
+                    Console.WriteLine(y);
+                    Console.WriteLine(x);
+                    Console.WriteLine(z);
+                }
+                else
+                {
+                    if (z < x && z < y && y < x)
+                    {
+                        Console.WriteLine(z);
+                        Console.WriteLine(y);
+                        Console.WriteLine(x);
+                    }
+                    else
+                    {
+                        if (z < x && z < y && x < y)
+                        {
+                            Console.WriteLine(z);
+                            Console.WriteLine(x);
+                            Console.WriteLine(y);
+                        }
+                        else
+                        {
+                            if (y < x && y < z && z < x)
+                            {
+                                Console.WriteLine(y);
+                                Console.WriteLine(z);
+                                Console.WriteLine(x);
+                            }
+                            else
+                            {
+                                if (x < y && x < z && z < y)
+                                {
+                                    Console.WriteLine(x);
+                                    Console.WriteLine(z);
+                                    Console.WriteLine(y);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
