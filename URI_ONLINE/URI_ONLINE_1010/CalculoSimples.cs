4fe3e3ef7e62170a5627d7d657d679d6f2b87dfe
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 01:28:44 -0300

    Exercício 1010(CalculoSimples) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1010/CalculoSimples.cs
@@ -0,0 +1,47 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1010
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1010
+{
+    class CalculoSimples
+    {
+        static void Main(string[] args)
+        {
+            int codigoPeca1;
+            int numeroPeca1;
+            double valorUnitario1;
+            double pagar;
+
+            int codigoPeca2;
+            int numeroPeca2;
+            double valorUnitario2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            codigoPeca1 = int.Parse(vet[0]);
+            numeroPeca1 = int.Parse(vet[1]);
+            valorUnitario1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            Console.ReadKey();
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            codigoPeca2 = int.Parse(vetor[0]);
+            numeroPeca2 = int.Parse(vetor[1]);
+            valorUnitario2 = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            pagar = valorUnitario1 * numeroPeca1 + valorUnitario2 * numeroPeca2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + pagar.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
