663474b3416cfee0a86151d228154f0106d981bf
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 13:57:27 -0300

    Exercício 1018(NumeroDeNotas) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1018/NumeroDeNotas.cs
@@ -0,0 +1,62 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1018
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1018
+{
+    class NumeroDeNotas
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int quociente;
+            int resto;
+            int nota;
+
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N);
+
+            resto = N;
+
+            nota = 100;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            nota = 50;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            nota = 20;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            nota = 10;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            nota = 5;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            nota = 2;
+            quociente = resto / nota;
+            Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
+            resto %= nota;
+
+            Console.WriteLine(resto + " nota(s) de R$ 1,00");
+            Console.ReadKey();
+        }
+    }
+}
