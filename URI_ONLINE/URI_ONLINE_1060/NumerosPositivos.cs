19de0e9826156660f51512575e625e727cfec622
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 17:35:52 -0300

    Exercício 1060(NumerosPositivos) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1060/NumerosPositivos.cs
@@ -0,0 +1,63 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1060
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1060
+{
+    class NumerosPositivos
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double C;
+            double D;
+            double E;
+            double F;
+            double positivo;
+            positivo = 0;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            D = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            E = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (A > 0)
+            {
+                positivo = positivo + 1;
+            }
+            if (B > 0)
+            {
+                positivo = positivo + 1;
+            }
+            if (C > 0)
+            {
+                positivo = positivo + 1;
+            }
+            if (D > 0)
+            {
+                positivo = positivo + 1;
+            }
+            if (E > 0)
+            {
+                positivo = positivo + 1;
+            }
+            if (F > 0)
+            {
+                positivo = positivo + 1;
+            }
+            Console.WriteLine(positivo + " valores positivos");
+            Console.ReadKey();
+        }
+    }
+}
