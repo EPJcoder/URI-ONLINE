bc16ca293d1529aa50a0d89563d638ad07b4dd86
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 15:54:02 -0300

    Exercício 1036(Bhaskara) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1036/Bhaskara.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1036
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+using System.Globalization;
+
+namespace URI_ONLINE_1036
+{
+    class Bhaskara
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double C;
+            double delta;
+            double r1;
+            double r2;
+
+            string[] vetor;
+            vetor = Console.ReadLine().Split(' ');
+            A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(B, 2.0) - 4 * A * C;
+
+            if (A == 0.0 || delta < 0.0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                r1 = (-B + Math.Sqrt(delta)) / (2.0 * A);
+                r2 = (-B - Math.Sqrt(delta)) / (2.0 * A);
+
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+            Console.ReadKey();
+        }
+    }
+}
