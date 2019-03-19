794d4b022964a2242025c274e3dce976f8cafd2d
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-19 17:29:49 -0300

    Exercício 1052(MesDoAno) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1052/MesDoAno.cs
@@ -0,0 +1,66 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1052
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1052
+{
+    class MesDoAno
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string mes;
+            mes = null;
+            x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    mes = "January";
+                    break;
+                case 2:
+                    mes = "February";
+                    break;
+                case 3:
+                    mes = "March";
+                    break;
+                case 4:
+                    mes = "April";
+                    break;
+                case 5:
+                    mes = "May";
+                    break;
+                case 6:
+                    mes = "June";
+                    break;
+                case 7:
+                    mes = "July";
+                    break;
+                case 8:
+                    mes = "August";
+                    break;
+                case 9:
+                    mes = "September";
+                    break;
+                case 10:
+                    mes = "October";
+                    break;
+                case 11:
+                    mes = "November";
+                    break;
+                case 12:
+                    mes = "December";
+                    break;
+            }
+            Console.WriteLine(mes);
+            Console.ReadKey();
+        }
+    }
+}
