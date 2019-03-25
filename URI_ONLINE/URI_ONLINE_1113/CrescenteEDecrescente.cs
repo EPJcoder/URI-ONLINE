1587e3596220b169a82bd2c7d24496309333ef5b
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-25 19:51:53 -0300

    Exercício 1113(CrescenteEDecrescente) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1113/CrescenteEDecrescente.cs
@@ -0,0 +1,46 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1113
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1113
+{
+    class CrescenteEDecrescente
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            int Y;
+
+            string[] vetor;
+            vetor = Console.ReadLine().Split(' ');
+
+            X = int.Parse(vetor[0]);
+            Y = int.Parse(vetor[1]);
+
+            while (X != Y)
+            {
+                if (X < Y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else if (X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                string[] vetor2;
+                vetor2 = Console.ReadLine().Split(' ');
+
+                X = int.Parse(vetor2[0]);
+                Y = int.Parse(vetor2[1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
