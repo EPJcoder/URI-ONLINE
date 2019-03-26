0b7a485b493705be602a89cab9edd5586e828719
EPJcoder <jogosdigitaissp@gmail.com>
2019-03-26 20:27:01 -0300

    Exercício 1114(SenhaFixa) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1114/SenhaFixa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_ONLINE_1114
+{
+    class SenhaFixa
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            int senha = 2002;
+
+            X = int.Parse(Console.ReadLine());
+
+            while (senha != X)
+            {
+                Console.WriteLine("Senha Invalida");
+                X = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadKey();
+        }
+    }
+}
