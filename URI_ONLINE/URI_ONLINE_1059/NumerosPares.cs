ef9ed52c71402c39ea8162f52461f28b70fdba75
EPJcoder <jogosdigitaissp@gmail.com>
2019-04-03 16:44:32 -0300

    Exercício 1059(NumerosPares) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1059/NumerosPares.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1059
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1059
+{
+    class NumerosPares
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                x = i * 2;
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
