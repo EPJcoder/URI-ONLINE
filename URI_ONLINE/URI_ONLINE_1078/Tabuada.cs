85d9df265c958953104e51198ffd845015a82ac2
EPJcoder <jogosdigitaissp@gmail.com>
2019-04-03 15:46:11 -0300

    Exercício 1078(Tabuada) adicionado ao projeto.

---
+++ URI_ONLINE/URI_ONLINE_1078/Tabuada.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *      Exercícios do site https://www.urionlinejudge.com.br
+ *      Programador: Euclydes Penteado Junior
+ *      Meu GitHub: EPJcoder
+ *      Linguagem utilizada: C#
+ *      Exercício de número: 1078
+ *      OBS: Para subir o exercício no URI apague o método: Console.ReadKey();
+ */
+
+using System;
+
+namespace URI_ONLINE_1078
+{
+    class Tabuada
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int tabuada;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                tabuada = i * N;
+                Console.WriteLine(i + " x " + N + " = " + tabuada);
+            }
+            Console.ReadKey();
+        }
+    }
+}
